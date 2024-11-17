f472e4fabbf3501cd8b9ddbafab3a4f22a1e9f02
Arqam Afaq <arqamafaq03@gmail.com>
2024-11-17 20:20:02 +0530

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
-  smooth: true
+  smooth: true,
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
@@ -13,24 +13,29 @@ locoScroll.on("scroll", ScrollTrigger.update);
 // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
   getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
   },
   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+  pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed",
 });
 
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
-
-
 
 /*nav-bar animation*/
 const timeline = gsap.timeline();
@@ -61,14 +66,14 @@ timeline.from("#showcase img", {
   stagger: 0.3,
 });
 timeline.from("#showcase .scrollMessage", {
-  x:-100 ,
+  x: -100,
   duration: 1,
   opacity: 0,
-  delay:2,
+  delay: 2,
 });
 
 gsap.from("#page1 img", {
-  x: (index) => (index === 0 ? "-100vw" : "100vw"),
+  x: (index) => (index === 0 ? "-170%" : "170%"),
   opacity: 0.7,
 
   scrollTrigger: {
@@ -83,7 +88,7 @@ gsap.from("#page1 img", {
 gsap.to("#jockerText h1", {
   transform: "translateX(-100%)",
   opacity: 1,
-  duration:1,
+  duration: 1,
   scrollTrigger: {
     trigger: "#jockerText",
     scroller: "#main",
@@ -113,17 +118,17 @@ gsap.to("#jockerText h1", {
 const galleryImages = document.querySelectorAll("#gallery img");
 
 galleryImages.forEach((img, index) => {
-    gsap.from(img, {
-        opacity: 0,
-        x: "-120%",
-        scrollTrigger: {
-            trigger: img,
-            scroller: "#main",
-            start: "top 80%",
-            end: "top 20%",
-            scrub: 1,
-            // markers: true, // Comment out in production
-            toggleActions: "play none none reverse"
-        }
-    });
+  gsap.from(img, {
+    opacity: 0,
+    x: "-120%",
+    scrollTrigger: {
+      trigger: img,
+      scroller: "#main",
+      start: "top 80%",
+      end: "top 20%",
+      scrub: 1,
+      // markers: true, // Comment out in production
+      toggleActions: "play none none reverse",
+    },
+  });
 });
